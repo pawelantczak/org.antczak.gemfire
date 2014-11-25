6e503ca4a32a11953b2d22c39743ee140e5b38ee
Paweł Antczak <pawel@antczak.org>
2014-11-25 21:16:32 +0000

    Server bootstrap

--- server/src/main/java/org/antczak/gemfire/server/Application.java
+++ server/src/main/java/org/antczak/gemfire/server/Application.java
@@ -1,21 +1,30 @@
 package org.antczak.gemfire.server;
 
+import com.gemstone.gemfire.cache.server.CacheServer;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.data.gemfire.repository.config.EnableGemfireRepositories;
 
 /**
  * Created by a516438 on 2014-11-25.
  */
 
 @SpringBootApplication
-//@ImportResource("/spring-data-gemfire-cache.xml")
-//@EnableGemfireRepositories
+@ImportResource("/spring-data-gemfire-server.xml")
+@EnableGemfireRepositories
 public class Application {
 
     Logger log = Logger.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    public void showPort(CacheServer cacheServer) {
+        log.info("Port number: " + cacheServer.getPort());
     }
 }
---
+++ server/src/main/java/org/antczak/gemfire/server/utils/ServerPortGenerator.java
@@ -0,0 +1,75 @@
+/* Copyright 2012 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.antczak.gemfire.server.utils;
+
+/**
+ * @author Wayne Lund
+ * @author David Turanski
+ *
+ */
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.security.SecureRandom;
+
+public class ServerPortGenerator {
+
+    Logger log = Logger.getLogger(ServerPortGenerator.class);
+
+    public int generatePort() {
+        SecureRandom random = new SecureRandom();
+        int port = random.nextInt(10000);
+        port += 40000;
+        // implement a check to make sure port is not used.
+        // on bind exception try again
+        log.debug("Server Port:" + port);
+        return port;
+    }
+
+    public int generatePort(int min, int max) throws IOException {
+        ServerSocket socket = new ServerSocket();
+        int port = bind(socket, min, max - min);
+        if (port > 0) {
+            socket.close();
+            return port;
+        } else {
+            throw new IOException("Unable to bind on to a port between " + min + " and " + max);
+        }
+
+    }
+
+    public int bind(ServerSocket socket, int portstart, int retries) throws IOException {
+        InetSocketAddress addr = null;
+        int port = portstart;
+        while (retries > 0) {
+            try {
+                addr = new InetSocketAddress(port);
+                socket.bind(addr);
+                retries = 0;
+                return port;
+            } catch (IOException x) {
+                retries--;
+                if (retries <= 0) {
+                    throw x;
+                }
+                port++;
+            }
+        }
+        return -1;
+    }
+
+}
